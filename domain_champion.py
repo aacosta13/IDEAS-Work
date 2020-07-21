17db14ce9f0070eb5417067d6beb1119fbbed467
Armando Acosta <armandoacosta96@gmail.com>
2020-07-20 19:21:44 -0700

    Added in analysis of commit files
    Function created to analyze files modified per commit by domain champions for each project.

--- domain_champion.py
+++ domain_champion.py
@@ -59,7 +59,64 @@ def retrieve_repo_info(uname, pword):
     print(df)
 
 
+def retrieve_file_info(uname, pwd):
+    commitfile = open('commit_info.txt', 'w')
+    commitfile.write('Repository Name/Top Committer/Commit Files Affected')
+    print(file=commitfile)
+
+    data = {'Repository': [],
+            'Committer': [],
+            'Commit Files Touched': []}
+
+    for project in project_dict:
+        print(' ', file=commitfile)
+        commit_url = 'https://api.github.com/repos/%s/commits' % project_dict[project]
+        contributor_url = 'https://api.github.com/repos/%s/contributors' % project_dict[project]
+
+        commit_response = requests.get(commit_url, auth=(uname, pwd))
+        commit_json_response = commit_response.json()
+        contributor_response = requests.get(contributor_url, auth=(uname, pwd))
+        contributor_json_response = contributor_response.json()
+
+        item_count = 0
+        page_no = 1
+        max_page = False
+        # Parse through JSON response of API for author
+        while not max_page:
+            for item in commit_json_response:
+                if item['author'] is None:
+                    item_count += 1
+                    continue
+
+                # If commit belongs to top dev, analyze the files here
+                if item['author']['login'] == contributor_json_response[0]['login']:
+                    individual_commit_url = 'https://api.github.com/repos/%s/commits' % project_dict[project] \
+                                            + '/' + str(item['sha'])
+                    individual_commit_response = requests.get(individual_commit_url, auth=(uname, pwd))
+                    individual_json_response = individual_commit_response.json()
+
+                    print('-----------------------------------')
+                    print('Developer: ', contributor_json_response[0]['login'])
+                    print('')
+                    print("Files modified by commit:")
+
+                    for file in individual_json_response['files']:
+                        print(file['filename'])
+                item_count += 1
+
+            if item_count == 30:
+                page_no += 1
+                commit_url += '?&page=%s' % str(page_no)
+                commit_response = requests.get(commit_url, auth=(uname, pwd))
+                commit_json_response = commit_response.json()
+                item_count = 0
+
+            else:
+                max_page = True
+
+
 if __name__ == '__main__':
     username = input('GitHub Authentication Username: ')
     password = input('Password: ')
     retrieve_repo_info(username, password)
+    retrieve_file_info(username, password)
